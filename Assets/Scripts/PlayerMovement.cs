1b93875ee5338ce7eac76502b373f7862acf585b
Christopher Edling <christopher.edling@sigma.se>
2019-01-16 08:10:36 +0100

    Migrate project to Windows

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,34 +1,35 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour {
-    Rigidbody2D rbody;
-    Animator anim;
-    private float movementSpeed;
-	// Use this for initialization
-	void Start () {
+public class PlayerMovement : MonoBehaviour
+{
+    private Animator anim;
+    public float movementSpeed = 10f;
+
+    private Rigidbody2D rbody;
+
+    // Use this for initialization
+    private void Start()
+    {
         rbody = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+    }
 
-	}
-	
-	// Update is called once per frame
-	void Update () {
-
-        Vector2 movement_vector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    // Update is called once per frame
+    private void Update()
+    {
+        var movement_vector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 
         if (movement_vector != Vector2.zero)
-            {
+        {
             anim.SetBool("isWalking", true);
             anim.SetFloat("input_x", movement_vector.x);
             anim.SetFloat("input_y", movement_vector.y);
-            }
+        }
         else
         {
             anim.SetBool("isWalking", false);
         }
 
-        rbody.MovePosition(rbody.position + movement_vector * Time.deltaTime * 100);
-	}
+        rbody.MovePosition(rbody.position + movement_vector * Time.deltaTime * movementSpeed * 10);
+    }
 }
